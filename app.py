596bf1bcf054011a0954c95120618e9ee4ceabc6
Vitor Pires <vitorsantopires@hotmail.com>
2024-11-01 13:13:34 -0300

    adicionando tema

--- app.py
+++ app.py
@@ -14,7 +14,23 @@ llm = GoogleGenerativeAI(model="gemini-1.5-flash-latest")
 st.set_page_config(page_title='Assistente Unih')
 st.header('Assistente Unih')
 
-col1, col2 = st.columns([3,1]) 
+st.markdown(
+    r"""
+    <style>
+    .stAppToolbar  {
+        visibility: hidden;
+    }
+    
+    div[data-testid="InputInstructions"] > span:nth-child(1) {
+        visibility: hidden;
+    }
+
+    [data-testid="stDecoration"] {background: linear-gradient(90deg, rgba(0,148,124,1) 0%, rgba(1,66,132,1) 100%);}
+    </style>
+    """, unsafe_allow_html=True
+)
+
+col1, col2 = st.columns([4,1]) 
 
 instr = ''
 with col1:
@@ -28,13 +44,10 @@ with col2:
     submitted = st.button('Enviar')
 
 if prompt and submitted:
-    with st.spinner('Carregando'):
+    with st.spinner('Carregando...'):
         newPrompt = llm.invoke(f'Use o texto a seguir para gerar uma frase com a ideia principal desse tema, que será utilizada para encontrar trechos de documentação numa busca vetorial. A frase deve estar em espanhol. Não dê instruções nem diga o que está fazendo.\n{prompt}')
         result = buscar_texto_similar(newPrompt)
-    with st.expander("Parâmetros"):
-        st.write(f'Texto original: {prompt}')
-        st.write(f'Busca vetorial feita a partir de: {newPrompt}\n')
-    with st.expander("Textos extraídos da documentação:"):
-        st.write(f'{result}')
-    with st.spinner('Carregando'):
+        #st.write(f'Texto original: {prompt}')
+        #st.write(f'Busca vetorial feita a partir de: {newPrompt}\n')
+        #t.write(f'{result}')
         st.write(f"{llm.invoke(f'Você é um agente da equipe de suporte, e receberá trechos de uma documentação que deve usar para responder a seguinte pergunta em português: {prompt}. {result}')}")
